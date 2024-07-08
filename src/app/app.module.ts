3b66e31b279f4238ebc0b9f71a0cff573b205d89
ptran999 <ptran@my365.bellevue.edu>
2024-07-08 02:54:42 -0700

    updateUser

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+/**
+ * Title: app.module.ts
+ * Author: Professor Krasso
+ * Date: 8/5/23
+ */
+
+// imports statements
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BaseLayoutComponent,
+    NavComponent,
+    FooterComponent,
+    NotFoundPageComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
